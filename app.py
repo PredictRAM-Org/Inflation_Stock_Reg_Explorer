8d603dbcefb3143969ffb9bb63d12c689894c115
PredictRAM DataHub <95133921+PredictRAM-Org@users.noreply.github.com>
2023-12-24 13:55:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,28 +22,45 @@ def analyze_stock(stock_data, cpi_data):
     # Merge stock and CPI data on Date
     merged_data = pd.merge(stock_data, cpi_data, left_index=True, right_index=True, how='inner')
     
+    # Calculate CPI change
+    merged_data['CPI Change'] = merged_data['CPI'].pct_change()
+    
     # Drop NaN values
     merged_data = merged_data.dropna()
 
-    # Show line chart with close price in blue and CPI in red
-    st.write("\nLine Chart - Close Price (Blue) and CPI (Red):")
-    plt.figure(figsize=(10, 6))
-    plt.plot(merged_data.index, merged_data['Close'], label='Close Price', color='blue')
-    plt.plot(merged_data.index, merged_data['CPI'], label='CPI', color='red')
-    
-    # Format x-axis as dates
-    plt.gca().xaxis_date()
+    # Features and target
+    X = merged_data[['CPI']]
+    y = merged_data['CPI Change']
 
-    plt.xlabel('Date')
-    plt.ylabel('Value')
-    plt.title('Line Chart - Close Price and CPI')
-    plt.legend()
+    # Train linear regression model
+    model = LinearRegression()
+    model.fit(X, y)
+
+    # Show correlation with each stock
+    correlation_results = {}
+    for column in merged_data.columns[:-2]:  # Exclude 'CPI Change' and 'CPI' columns
+        stock_column = merged_data[column]
+        correlation = stock_column.corr(merged_data['CPI Change'])
+        correlation_results[column] = correlation
+
+    # Display results
+    st.write("\nCorrelation with CPI Change:")
+    for stock, correlation in correlation_results.items():
+        st.write(f"{stock}: {correlation}")
+
+    # Show correlation heatmap
+    correlation_matrix = merged_data.corr()
+    sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm")
     st.pyplot()
 
 # Streamlit UI
-st.title("Stock-CPI Line Chart - Close Price and CPI")
-selected_stock = st.selectbox("Select Stock", stock_files)
+st.title("Stock-CPI Correlation Analysis")
+train_model_button = st.button("Train Model")
 
-# Load selected stock data
-selected_stock_data = pd.read_excel(os.path.join(stock_folder, selected_stock))
-analyze_stock(selected_stock_data, cpi_data)
+if train_model_button:
+    st.write("Training model...")
+
+    for stock_file in stock_files:
+        st.write(f"\nCorrelation for {stock_file}:")
+        selected_stock_data = pd.read_excel(os.path.join(stock_folder, stock_file))
+        analyze_stock(selected_stock_data, cpi_data)
