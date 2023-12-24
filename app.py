af504f7e22946681f8ebc4ebc1badeb07985cd83
PredictRAM DataHub <95133921+PredictRAM-Org@users.noreply.github.com>
2023-12-24 14:13:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,69 +15,51 @@ stock_folder = "stock_folder"
 stock_files = [f for f in os.listdir(stock_folder) if f.endswith(".xlsx")]
 
 # Function to calculate correlation and build linear regression model
-def analyze_stock(stock_data, cpi_data, future_inflation):
+def analyze_stock(stock_data, cpi_data, expected_inflation):
     stock_data['Date'] = pd.to_datetime(stock_data['Date'])
     stock_data.set_index('Date', inplace=True)
     
     # Merge stock and CPI data on Date
     merged_data = pd.merge(stock_data, cpi_data, left_index=True, right_index=True, how='inner')
     
+    # Handle NaN values in CPI column
+    if merged_data['CPI'].isnull().any():
+        st.write(f"Warning: NaN values found in 'CPI' column for {stock_data.name}. Dropping NaN values.")
+        merged_data = merged_data.dropna(subset=['CPI'])
+
     # Calculate CPI change
     merged_data['CPI Change'] = merged_data['CPI'].pct_change()
-    
-    # Drop NaN values
+
+    # Drop NaN values after calculating percentage change
     merged_data = merged_data.dropna()
 
-    # Features and target
-    X = merged_data[['CPI']]
-    y = merged_data['CPI Change']
+    # Show correlation between 'Close' column and 'CPI Change'
+    correlation_close_cpi = merged_data['Close'].corr(merged_data['CPI Change'])
+    st.write(f"Correlation between 'Close' and 'CPI Change' for {stock_data.name}: {correlation_close_cpi}")
 
-    # Train linear regression model
-    model = LinearRegression()
-    model.fit(X, y)
+    # Calculate the adjusted correlation based on expected inflation impact
+    adjusted_correlation = correlation_close_cpi + 0.1 * expected_inflation  # You can adjust the multiplier as needed
+    st.write(f"Adjusted Correlation with Expected Inflation ({expected_inflation}): {adjusted_correlation}")
 
-    # Predict future inflation
-    future_inflation_prediction = model.predict([[future_inflation]])
-    st.write(f"Predicted CPI Change for Future Inflation ({future_inflation}): {future_inflation_prediction[0]}")
-
-    # Show correlation with actual CPI Change
-    correlation_results_actual = {}
-    for column in merged_data.columns[:-2]:  # Exclude 'CPI Change' and 'CPI' columns
-        stock_column = merged_data[column]
-        correlation = stock_column.corr(merged_data['CPI Change'])
-        correlation_results_actual[column] = correlation
-
-    # Show correlation with predicted CPI Change
-    correlation_results_predicted = {}
-    for column in merged_data.columns[:-2]:  # Exclude 'CPI Change' and 'CPI' columns
-        stock_column = merged_data[column]
-        correlation = stock_column.corr(pd.Series(future_inflation_prediction[0], index=stock_column.index))
-        correlation_results_predicted[column] = correlation
-
-    # Display results
-    st.write("\nCorrelation with Actual CPI Change:")
-    for stock, correlation in correlation_results_actual.items():
-        st.write(f"{stock}: {correlation}")
-
-    st.write("\nCorrelation with Predicted CPI Change:")
-    for stock, correlation in correlation_results_predicted.items():
-        st.write(f"{stock}: {correlation}")
-
-    # Show correlation heatmap
-    correlation_matrix = merged_data.corr()
-    sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm")
-    st.pyplot()
+    return adjusted_correlation
 
 # Streamlit UI
-st.title("Stock-CPI Correlation and Prediction Analysis")
-selected_stock = st.selectbox("Select Stock", stock_files)
-future_inflation_input = st.number_input("Enter Expected Future Inflation:", min_value=0.0, step=0.01)
+st.title("Stock-CPI Correlation Analysis with Expected Inflation")
+expected_inflation = st.number_input("Enter Expected Upcoming Inflation:", min_value=0.0, step=0.01)
 train_model_button = st.button("Train Model")
 
-# Load selected stock data
-selected_stock_data = pd.read_excel(os.path.join(stock_folder, selected_stock))
-
-# Train the model if the button is clicked
 if train_model_button:
-    st.write("Training model...")
-    analyze_stock(selected_stock_data, cpi_data, future_inflation_input)
+    st.write(f"Training model with Expected Inflation: {expected_inflation}...")
+    
+    all_correlations = []
+    for stock_file in stock_files:
+        st.write(f"\nTraining for {stock_file}...")
+        selected_stock_data = pd.read_excel(os.path.join(stock_folder, stock_file))
+        selected_stock_data.name = stock_file  # Assign a name to the stock_data for reference
+        correlation = analyze_stock(selected_stock_data, cpi_data, expected_inflation)
+        all_correlations.append((stock_file, correlation))
+
+    # Display overall summary
+    st.write("\nOverall Summary:")
+    for stock, correlation in all_correlations:
+        st.write(f"{stock}: {correlation}")
